63af239d6b578bd4855f3f95265277e6682d27cd
Ian <ianbanks123@yahoo.co.uk>
2012-04-07 21:15:03 +0100

    adding code files

---
+++ AMFCT.PublishAlphas.js
@@ -0,0 +1,63 @@
+var amfct = this.amfct || {};
+
+amfct.PublishAlphas = function () {
+    var that, domEvents, selectAllAlphas, stringEndsWith, msgPrompt, dataObject, ajaxRequest, validatePublishAlphas, formChanged;
+    that = this;
+
+    /*Dependencies*/
+    msgPrompt = new amfct_utils.MessagePrompt();
+	validatePublishAlphas = new amfct_utils.ValidationSummary();
+    ajaxRequest = new amfct_utils.AjaxCalls();
+	dataObject = new amfct_utils.DataConstructor();
+
+    /*Private Functions*/
+    domEvents = function () {
+        that.container.find('form').submit(function () { return false; });
+        that.publishBtn.bind('click', that.PublishSelectedAlphas);
+        that.selectAll.delegate('a', 'click', selectAllAlphas);
+		that.seasonInput.bind('keyup', function () { formChanged = true; });
+    };
+    selectAllAlphas = function (event) {
+        event.preventDefault();
+        if ($(this).attr('href') === '#selectAll') {
+            $(this).parents('fieldset').find('input[type="checkbox"]').attr('checked', true);
+        } else {
+            $(this).parents('fieldset').find('input[type="checkbox"]').attr('checked', false);
+        }
+    };
+	requestCallback = function () {
+        msgPrompt.init('msg', 'loading', '<img src="/assets/images/core/ajax-loader.gif" alt="">', 'body', 'bottom');
+    };
+	publishSuccessCallback = function (response) {
+		msgPrompt.TearDownMsg('#msg');
+		msgPrompt.init('msg','publishing-successful', '<p>Alpha\'s have successfully been published</p>', 'body', 'top', true);
+        that.dataRow.parents('#search-results').addClass('is-saved');
+    };
+    publishFailureCallback = function (jqXHR, textStatus, errorThrown) {
+        console.log(jqXHR);
+		msgPrompt.TearDownMsg('#msg');
+		msgPrompt.init('msg', 'publishing-unsuccessful', '<p>A system error has occured, Alpha\'s have not been published</p><a class="close" href="#close">Close</a>', 'body', 'bottom');
+    };
+
+    /*Public Functions*/
+    this.init = function (container) {
+        this.container = $(container);
+        this.selectAll = this.container.find('.select-episodes');
+        this.dataRow = this.container.find('.episode-details tbody tr');
+		this.seasonInput = this.container.find('.season-no input');
+      	this.publishBtn = $('#publish');
+        domEvents();
+    };
+    this.PublishSelectedAlphas = function (event) {
+        event.preventDefault();
+        var validateAlphas, jsonData;
+		validateAlphas =  validatePublishAlphas.init(that.dataRow, '<p>Please complete the missing fields for the AE number you are trying to submit(listed below) before clicking \'Publish\' again.</p>', formChanged);
+        if (validatePublishAlphas.CheckAlphas()) {
+            jsonData = dataObject.ParseFieldInputs();
+			console.log(jsonData);
+            ajaxRequest.SendData('/Home/Publish', jsonData, requestCallback, publishSuccessCallback, publishFailureCallback);
+        } else {
+            return false;
+        }
+    };
+};
---
+++ AMFCT.Utils.DataConstructor.js
@@ -0,0 +1,71 @@
+var amfct_utils = this.amfct_utils || {};
+amfct_utils.DataConstructor = function () {
+	var that, ParseChangedRows, ParseInputElements, isArray, stringEndsWith;
+	that = this;
+	
+	/*Private Functions*/
+	isArray = function(o) {
+		return Object.prototype.toString.call(o) === '[object Array]';
+	};
+	stringEndsWith = function (input, suffix) {
+        return input.indexOf(suffix, input.length - suffix.length) !== -1;
+    };
+	ParseChangedRows = function (rows) {
+        var rowValues;
+        var storedInputs;
+        rowValues = [];
+        $(rows).find('input:checked').each(function () {
+            storedInputs = $(this).parents('tr').find(':input').not('input[type="checkbox"]');
+            rowValues[rowValues.length] = ParseInputElements(storedInputs);
+        });
+        return rowValues;
+    };
+	ParseInputElements = function (inputElems) {
+        var values = {};
+		$(inputElems).each(function (index) {
+            var name = $(this).attr('name');
+            if (stringEndsWith(name, '[]')) {
+                if (!isArray(values[name]))
+                    values[name] = [];
+                var arraySize = values[name].length;
+                values[name][arraySize] = $(this).val();
+            }
+            else {
+                values[name] = $(this).val();
+            }
+        });
+        return values;
+    };    
+	
+	/*Public Functions*/
+	this.ParseFieldInputs = function () {
+		var jsonString, series, seasonInput, storedSeason;
+        series = [];
+        storedSeason = {};
+        seasonInput = $('.episode-details');
+		seasonInput.each(function () {
+            var hasCheckedAlphas, alphaData, serie;
+            alphaData = [];
+            serie = {};
+			hasCheckedAlphas = $(this).find('input:checked').val() ? true : false;
+            if (hasCheckedAlphas) {
+				var aeNumber;
+             	aeNumber = $(this).attr('id');
+                serie["SeasonNumber"] = $(this).parent().find('.series-details').find('input[type="text"]').val();
+                serie["TitleTk"] = $(this).parent().find('.series-details').find('input[type="hidden"]').val();
+                if (!storedSeason[aeNumber] && $(this).find('input:checked')) {
+                    storedSeason[aeNumber] = true;
+                    alphaData = ParseChangedRows(this);
+                    serie['AlphaChanges'] = alphaData;
+                    series[series.length] = serie;
+                }
+            } else {
+				serie["SeasonNumber"] = $(this).parent().find('.series-details').find('input[type="text"]').val();
+				serie["TitleTk"] = $(this).parent().find('.series-details').find('input[type="hidden"]').val();
+			}
+			series[series.length] = serie;
+        });
+        jsonString = '{"SeriesChanges":' + JSON.stringify(series) + '}';
+       	return jsonString;
+    };
+};  
