e395bf9b8ec529beedcea720ac47a05aae0fc0a0
Ian Banks <ianbanks123@yahoo.co.uk>
2012-04-09 22:32:45 +0200

    Update AMFCT.Utils.DataConstructor.js

--- AMFCT.Utils.DataConstructor.js
+++ AMFCT.Utils.DataConstructor.js
@@ -8,64 +8,63 @@ amfct_utils.DataConstructor = function () {
 		return Object.prototype.toString.call(o) === '[object Array]';
 	};
 	stringEndsWith = function (input, suffix) {
-        return input.indexOf(suffix, input.length - suffix.length) !== -1;
-    };
+        	return input.indexOf(suffix, input.length - suffix.length) !== -1;
+    	};
 	ParseChangedRows = function (rows) {
-        var rowValues;
-        var storedInputs;
-        rowValues = [];
-        $(rows).find('input:checked').each(function () {
-            storedInputs = $(this).parents('tr').find(':input').not('input[type="checkbox"]');
-            rowValues[rowValues.length] = ParseInputElements(storedInputs);
-        });
-        return rowValues;
-    };
+        	var rowValues;
+        	var storedInputs;
+        	rowValues = [];
+        	$(rows).find('input:checked').each(function () {
+            		storedInputs = $(this).parents('tr').find(':input').not('input[type="checkbox"]');
+            		rowValues[rowValues.length] = ParseInputElements(storedInputs);
+        	});
+        	return rowValues;
+    	};
 	ParseInputElements = function (inputElems) {
-        var values = {};
+        	var values = {};
 		$(inputElems).each(function (index) {
-            var name = $(this).attr('name');
-            if (stringEndsWith(name, '[]')) {
-                if (!isArray(values[name]))
-                    values[name] = [];
-                var arraySize = values[name].length;
-                values[name][arraySize] = $(this).val();
-            }
-            else {
-                values[name] = $(this).val();
-            }
-        });
-        return values;
-    };    
+        		var name = $(this).attr('name');
+            		if (stringEndsWith(name, '[]')) {
+                		if (!isArray(values[name]))
+                   			values[name] = [];
+                		var arraySize = values[name].length;
+                		values[name][arraySize] = $(this).val();
+            		} else {
+                		values[name] = $(this).val();
+            		}
+        	});
+        	return values;
+    	};    
 	
 	/*Public Functions*/
 	this.ParseFieldInputs = function () {
 		var jsonString, series, seasonInput, storedSeason;
-        series = [];
-        storedSeason = {};
-        seasonInput = $('.episode-details');
+        	series = [];
+       	 	storedSeason = {};
+        	seasonInput = $('.episode-details');
 		seasonInput.each(function () {
-            var hasCheckedAlphas, alphaData, serie;
-            alphaData = [];
-            serie = {};
+            		var hasCheckedAlphas, alphaData, serie;
+            		alphaData = [];
+            		serie = {};
 			hasCheckedAlphas = $(this).find('input:checked').val() ? true : false;
-            if (hasCheckedAlphas) {
+            		if (hasCheckedAlphas) {
 				var aeNumber;
-             	aeNumber = $(this).attr('id');
-                serie["SeasonNumber"] = $(this).parent().find('.series-details').find('input[type="text"]').val();
-                serie["TitleTk"] = $(this).parent().find('.series-details').find('input[type="hidden"]').val();
-                if (!storedSeason[aeNumber] && $(this).find('input:checked')) {
-                    storedSeason[aeNumber] = true;
-                    alphaData = ParseChangedRows(this);
-                    serie['AlphaChanges'] = alphaData;
-                    series[series.length] = serie;
-                }
-            } else {
+             			aeNumber = $(this).attr('id');
+                		serie["SeasonNumber"] = $(this).parent().find('.series-details').find('input[type="text"]').val();
+                		serie["TitleTk"] = $(this).parent().find('.series-details').find('input[type="hidden"]').val();
+                		if (!storedSeason[aeNumber] && $(this).find('input:checked')) {
+                    			storedSeason[aeNumber] = true;
+                    			alphaData = ParseChangedRows(this);
+                    			serie['AlphaChanges'] = alphaData;
+                    			series[series.length] = serie;
+                		}
+           		 } else {
 				serie["SeasonNumber"] = $(this).parent().find('.series-details').find('input[type="text"]').val();
 				serie["TitleTk"] = $(this).parent().find('.series-details').find('input[type="hidden"]').val();
 			}
 			series[series.length] = serie;
-        });
-        jsonString = '{"SeriesChanges":' + JSON.stringify(series) + '}';
-       	return jsonString;
-    };
+        	});
+        	jsonString = '{"SeriesChanges":' + JSON.stringify(series) + '}';
+       		return jsonString;
+    	};
 };  
