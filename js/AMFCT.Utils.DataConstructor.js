ca3dbd9f50c88440fa30133efb632b019c1de44d
Ian <ianbanks123@yahoo.co.uk>
2017-06-02 20:51:55 +0100

    pull request test

--- js/AMFCT.Utils.DataConstructor.js
+++ js/AMFCT.Utils.DataConstructor.js
@@ -5,6 +5,7 @@ amfct_utils.DataConstructor = function () {
 	
 	/*Private Functions*/
 	isArray = function(o) {
+		console.log('hello');
 		return Object.prototype.toString.call(o) === '[object Array]';
 	};
 	stringEndsWith = function (input, suffix) {
